a439a2d02796c9565fe2818672d2d59f58af15f8
Rui <am_crrr@qq.com>
2018-04-23 17:05:40 +0800

    machine learning completed

--- biodataprocess/scale.py
+++ biodataprocess/scale.py
@@ -1,19 +1,20 @@
 import numpy as np
 import pandas as pd
-from ete3 import Tree
 
-
-from sklearn import preprocessing
 def max_min_scale(filename):
     filename_ = filename+'.xls'
     a = pd.read_table(filename_)
     index = a['name']
     a = a.set_index('name')
-    scaler = preprocessing.MinMaxScaler()
-    a_scaler = scaler.fit_transform(a)
-    c = pd.DataFrame(a_scaler,index= index,columns=index)
+    pre_data = np.array(a)
+    amin, amax = pre_data.min(), pre_data.max()
+    pre_data = (pre_data - amin) / (amax - amin)
+    print(pre_data)
+    c = pd.DataFrame(pre_data,index= index,columns=index)
     c.to_excel(filename+'_scaled.xls')
     return c
-matrix = max_min_scale('coor_dm')
 
+max_min_scale('coor_dm')
+max_min_scale('Hmarker_Distance_Matrix')
+max_min_scale('snvNaNreplacedm')
 
---
+++ biodataprocess/test.py
@@ -0,0 +1,16 @@
+
+
+
+
+a = [11,19,23]
+b = 53
+
+for i in a:
+    b = b % i
+if b == 0:
+    print(True)
+else:
+    print(False)
+
+
+
