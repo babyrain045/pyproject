1dd99f4172fd2389003b536e007d2d165928de0b
babyrain045 <am_crrr@qq.com>
2019-02-28 11:52:04 +0000

    update

--- leetcode/ContainMostWater.py
+++ leetcode/ContainMostWater.py
@@ -2,27 +2,31 @@
 
 a_list = [1,8,6,2,5,4,8,3,7]
 lenth = len(a_list)
-count = 0
+count = 1
 r_index = -1
-for i in a_list:
-    if count < lenth/2:
-        count = count + 1;
-        l_flag = i
-        r_flag = a_list[-count]
-        r_wid = lenth
-        if count == 1:
-            area = (lenth - count) * i
-        elif i < a_list[r_index]:
-            continue
+r_flag = a_list[r_index]
+area = 0
+while(count - r_index <= lenth):
+        if a_list[count-1] < a_list[r_index]:
+            print(a_list[count-1],a_list[r_index])
+            r_wid = lenth + r_index + 1
+            up_area = (r_wid - count) * a_list[count-1]
+            if up_area > area:
+                area = up_area
+            count = count + 1
+            print("jishu:",count)
+            print(a_list[count-1],r_flag)
+            print("zuobian area:",up_area,a_list[count - 1],r_wid,count)
         else:
             r_wid = r_index + lenth + 1
             r_flag = a_list[r_index]
             up_area = (r_wid - count) * r_flag
-            print(i,r_flag)
-            print(up_area)
+            print(a_list[count-1],r_flag)
+            print("youbian area:",up_area)
             if up_area > area:
                 area = up_area
             r_index = r_index - 1
+
 print(area)
 
 
--- test.py
+++ test.py
@@ -1,3 +1,25 @@
+import sys
+count = 0
+for line in sys.stdin:
+    count = count + 1
+    if count == 1:
+        stu_cout, ope_cout = line.strip('\n').split()
+        stu_cout = int(stu_cout)
+        ope_cout = int(ope_cout)
+    elif count == 2:
+        stu_num = line.strip('\n').split()
+        stu_list = []
+        for i in stu_num:
+            stu_list.append(int(i))
+        print(stu_list)
+    else:
+        judge_wd,ope_index,ope_val = line.split()
+        ope_index = int(ope_index)
+        ope_val = int(ope_val)
+        if judge_wd == 'Q':
+            stu_ran = stu_list[ope_index-1 : ope_val]
+            print(max(stu_ran))
+        elif judge_wd == 'U':
+            stu_list[ope_index-1] = ope_val
 
 
-print("hello world!")
