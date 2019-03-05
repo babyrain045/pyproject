9943846d32055c384a16f21a045601253522fea8
babyrain045 <am_crrr@qq.com>
2019-03-05 13:58:55 +0000

    update mey leetcode solution

--- test.py
+++ test.py
@@ -1,25 +1,28 @@
-import sys
-count = 0
-for line in sys.stdin:
-    count = count + 1
-    if count == 1:
-        stu_cout, ope_cout = line.strip('\n').split()
-        stu_cout = int(stu_cout)
-        ope_cout = int(ope_cout)
-    elif count == 2:
-        stu_num = line.strip('\n').split()
+
+while True:
+    try:
+        stu_ope_list = input().split()
+        stu_cout = int(stu_ope_list[0])
+        ope_cout = int(stu_ope_list[1])
+        stu_num = input().split()
         stu_list = []
+        count = 0
         for i in stu_num:
             stu_list.append(int(i))
-        print(stu_list)
-    else:
-        judge_wd,ope_index,ope_val = line.strip('\n').split()
-        ope_index = int(ope_index)
-        ope_val = int(ope_val)
-        if judge_wd == 'Q':
-            stu_ran = stu_list[ope_index-1 : ope_val]
-            print(max(stu_ran))
-        elif judge_wd == 'U':
-            stu_list[ope_index-1] = ope_val
+        while count <= ope_cout:
+            count = count + 1
+            judge_wd,ope_index,ope_val = input().split()
+            ope_index = int(ope_index)
+            ope_val = int(ope_val)
+            if judge_wd == 'Q':
+                start = min(ope_index-1,ope_val)
+                end = max(ope_index-1,ope_val)
+                stu_ran = stu_list[start : end]
+                print(max(stu_ran))
+            elif judge_wd == 'U':
+                stu_list[ope_index-1] = ope_val
+    except:
+        break
+
 
 
